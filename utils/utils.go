0bdc3148209683052f5b8d972d15da80f83b028b
Junko Enoshima <156222018+2dChan@users.noreply.github.com>
2026-01-20 13:12:41 +0300

    feat(utils): add point generator in S2

---
+++ utils/utils.go
@@ -0,0 +1,27 @@
+// Copyright (c) 2026 Andrey Kriulin
+// Licensed under the MIT License.
+// See the LICENSE file in the project root for full license text.
+
+package utils
+
+import (
+	"math"
+	"math/rand"
+
+	"github.com/golang/geo/s1"
+	"github.com/golang/geo/s2"
+)
+
+func GenerateRandomPoints(cnt int, seed int64) s2.PointVector {
+	random := rand.New(rand.NewSource(seed))
+	sites := make(s2.PointVector, cnt)
+
+	for i := range cnt {
+		sites[i] = s2.PointFromLatLng(s2.LatLng{
+			Lat: s1.Angle((random.Float64() - 0.5) * math.Pi),
+			Lng: s1.Angle((random.Float64()*2 - 1) * math.Pi),
+		})
+	}
+
+	return sites
+}
